9c485b790b6d1884e930aacd999c4e08f343f1e0
Maksim Kishinskiy <m.kishinskiy@gmail.com>
2020-05-11 23:15:19 -0400

    add arguments for commandline use

--- mac_changer.py
+++ mac_changer.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface = raw_input("interface >> ")
-new_mac = raw_input("new MAC >> ")
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
